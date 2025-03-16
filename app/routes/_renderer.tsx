ce88246be3ec3cbf2a80e52d6cf31967e8f041e2
reamkf <68076678+reamkf@users.noreply.github.com>
2025-03-16 19:26:31 +0900

    Fix interaction not working

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { reactRenderer } from '@hono/react-renderer'
 import { HasIslands, Link } from 'honox/server'
 
-export default jsxRenderer(({ children }) => {
+export default reactRenderer(({ children }) => {
 	return (
 		<html lang='ja'>
 			<head>
-				<meta charset='UTF-8' />
+				<meta charSet='UTF-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<Link href='/app/style.css' rel='stylesheet' />
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,45 @@
 import { defineConfig } from 'vite'
 import honox from 'honox/vite'
 import build from '@hono/vite-build/cloudflare-workers'
+import adapter from "@hono/vite-dev-server/cloudflare";
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-	plugins: [
-		honox({
-			client: {
-				input: ['/app/style.css'],
+export default defineConfig(({ mode }) => {
+	if (mode === 'client') {
+		return {
+			build: {
+				rollupOptions: {
+					input: ['./app/client.ts', './app/style.css'],
+					output: {
+						entryFileNames: 'static/client.js',
+						chunkFileNames: 'static/assets/[name]-[hash].js',
+						assetFileNames: 'static/assets/[name].[ext]'
+					}
+				},
+				emptyOutDir: false
 			},
-		}),
-		build(),
-		tailwindcss(),
-	],
-	ssr: {
-		external: ['react', 'encoding-japanese'],
-	},
+			plugins: [
+				tailwindcss(),
+			],
+			ssr: {
+				external: ['encoding-japanese'],
+			},
+		}
+	} else {
+		return {
+			ssr: {
+				external: ['react', 'react-dom', 'encoding-japanese'],
+			},
+			plugins: [
+				honox({
+					devServer: { adapter},
+					client: {
+						input: ['/app/style.css'],
+					},
+				}),
+				tailwindcss(),
+				build(),
+			],
+		}
+	}
 })
