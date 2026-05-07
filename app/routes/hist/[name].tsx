724b9b2c5ce6f81c23eccacd556b1aeec77d3869
reamkf <68076678+reamkf@users.noreply.github.com>
2026-05-07 23:20:25 +0900

    refactor: Remove duplicate code for hist route

--- app/routes/hist/[name].tsx
+++ app/routes/hist/[name].tsx
@@ -1,10 +1,3 @@
-import { createRoute } from 'honox/factory'
-import { getPageHistoryUrl } from 'seesaawiki-url';
-import { SEESAA_WIKI_BASE_URL } from '../../utils/seesaawiki-base-url'
-
-export default createRoute((c) => {
-	const name = c.req.param('name');
-	const url = getPageHistoryUrl(SEESAA_WIKI_BASE_URL, name);
-
-	return c.redirect(url)
-})
+// Alias to /history/[name]
+import createRoute from '../history/[name]'
+export default createRoute;
